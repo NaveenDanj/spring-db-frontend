dc40bb118d6ebadfe52e2773609d7fad61a1c29c
Naveen Dhananjaya <naveenhettiwaththa@gmail.com>
2022-07-10 18:54:04 +0530

    added basic dashboard layout

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,26 +1,59 @@
 <template>
-  <v-app-bar app color="#010203" dark>
-    <div class="align-center">
-      <h3>Dashboard</h3>
-      <label style="color: #ff7750">Hi there,</label
-      ><label> Great to see you again</label>
-    </div>
+  <div>
+    <v-app-bar app color="#010203" dark elevation="0">
+        <v-btn fab small class="mr-5" @click="() => drawer = !drawer">
+          <v-icon>mdi-menu</v-icon>
+        </v-btn>
+      <div class="align-center">
+        <h3>Dashboard</h3>
+        <label style="color: #ff7750">Hi there,</label
+        ><label> Great to see you again</label>
+      </div>
 
-    <v-spacer></v-spacer>
+      <v-spacer></v-spacer>
+      
+    </v-app-bar>
 
-    <v-btn
-      href="https://github.com/vuetifyjs/vuetify/releases/latest"
-      target="_blank"
-      text
-    >
-      <span class="mr-2">Latest Release</span>
-      <v-icon>mdi-open-in-new</v-icon>
-    </v-btn>
-  </v-app-bar>
+    <v-navigation-drawer app v-model="drawer" dark color="#010203">
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="text-h6"> SpringDB </v-list-item-title>
+          <v-list-item-subtitle> @Username </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list dense nav>
+        <v-list-item v-for="item in items" :key="item.title" link>
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+  </div>
 </template>
 
 <script>
 export default {
   name: "Home",
+  data() {
+    return {
+      drawer : true,
+      items: [
+        { title: "Dashboard", icon: "mdi-view-dashboard" },
+        { title: "Backups", icon: "mdi-image" },
+        { title: "App Manager", icon: "mdi-help-box" },
+        { title: "Users", icon: "mdi-help-box" },
+        { title: "Instance", icon: "mdi-help-box" },
+      ],
+      right: null,
+    };
+  },
 };
 </script>
