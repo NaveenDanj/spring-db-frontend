58425ca4ef0941b51ebee1ed26517ef6f972303f
Naveen Dhananjaya <naveenhettiwaththa@gmail.com>
2022-07-13 06:24:11 +0530

    added add app dialog

---
+++ src/components/Dialog/AppManager/AddAppManagerDialog.vue
@@ -0,0 +1,67 @@
+<template>
+    <v-dialog v-model="dialog" persistent width="700px" dark>
+
+        <template v-slot:activator="{ on, attrs }">
+            <v-btn
+                rounded
+                color="#FF7750"
+                dark
+                v-bind="attrs"
+                v-on="on"
+
+            >
+                Add new App
+            </v-btn>
+        </template>
+
+        <v-card color="#080E15">
+            <v-toolbar color="#161635" elevation="0">
+                <v-toolbar-title>Add new App</v-toolbar-title>
+                <v-spacer />
+                <v-btn fab text @click="dialog = false">
+                    <v-icon>mdi-close</v-icon>
+                </v-btn>
+            </v-toolbar>
+
+            <v-card-text class="pa-4">
+
+                <form class="mt-2">
+
+                    <div>
+                        <v-text-field outlined dense label="App Name" value="POS APP" />
+                    </div>
+
+                    <div>
+                        <v-row>
+
+                            <v-col cols="12" md="8">
+                                <v-text-field outlined dense label="APP KEY" value="POS APP" />
+                            </v-col>
+
+                            <v-col  cols="12" md="4">
+                                <v-btn rounded outlined color="success">Generate</v-btn>
+                            </v-col>
+
+                        </v-row>
+                    </div>
+
+                    <v-btn color="success" rounded>ADD</v-btn>
+                    <v-btn color="red" class="ml-2" rounded>CANCEL</v-btn>
+
+                </form>
+
+
+            </v-card-text>
+        </v-card>
+    </v-dialog>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        dialog: false,
+      }
+    },
+  }
+</script>
--- src/views/Dashboard/AppManager.vue
+++ src/views/Dashboard/AppManager.vue
@@ -3,7 +3,7 @@
     <v-card color="#080E15" dark class="pa-2">
       <div class="d-flex justify-space-between">
         <h2 class="white--text mb-3">App Manager</h2>
-        <v-btn rounded color="#FF7750">Add new App</v-btn>
+        <AddAppManagerDialog />
       </div>
 
       <v-divider />
@@ -43,63 +43,65 @@
 </template>
 
 <script>
+import AddAppManagerDialog from "../../components/Dialog/AppManager/AddAppManagerDialog.vue";
 export default {
-  data() {
-    return {
-      page: 1,
-      headers: [
-        {
-          text: "App Name",
-          align: "start",
-          sortable: false,
-          value: "name",
-        },
-        { text: "Status", value: "status", sortable: false },
-        { text: "Last Run", value: "last", sortable: false },
-        { text: "Runs every", value: "every", sortable: false },
-        { text: "Runs on", value: "on", sortable: false },
-        { text: "Total Backups", value: "total", sortable: false },
-        { text: "Action", value: "action", sortable: false },
-      ],
-      desserts: [
-        {
-          name: "Test App",
-          status: "Active",
-          role: "Admin",
-          last: "2018/03/29 @07:25",
-          every: "1 hour",
-          on: "Monday",
-          total: "10",
-        },
-        // {
-        //   name: "Test App",
-        //   status: "Active",
-        //   role: "Admin",
-        //   last: "2018/03/29 @07:25",
-        //   every: "1 hour",
-        //   on: "Monday",
-        //   total: "10",
-        // },
-        // {
-        //   name: "Test App",
-        //   status: "Active",
-        //   role: "Admin",
-        //   last: "2018/03/29 @07:25",
-        //   every: "1 hour",
-        //   on: "Monday",
-        //   total: "10",
-        // },
-        // {
-        //   name: "Test App",
-        //   status: "Active",
-        //   role: "Admin",
-        //   last: "2018/03/29 @07:25",
-        //   every: "1 hour",
-        //   on: "Monday",
-        //   total: "10",
-        // },
-      ],
-    };
-  },
+    data() {
+        return {
+            page: 1,
+            headers: [
+                {
+                    text: "App Name",
+                    align: "start",
+                    sortable: false,
+                    value: "name",
+                },
+                { text: "Status", value: "status", sortable: false },
+                { text: "Last Run", value: "last", sortable: false },
+                { text: "Runs every", value: "every", sortable: false },
+                { text: "Runs on", value: "on", sortable: false },
+                { text: "Total Backups", value: "total", sortable: false },
+                { text: "Action", value: "action", sortable: false },
+            ],
+            desserts: [
+                {
+                    name: "Test App",
+                    status: "Active",
+                    role: "Admin",
+                    last: "2018/03/29 @07:25",
+                    every: "1 hour",
+                    on: "Monday",
+                    total: "10",
+                },
+                // {
+                //   name: "Test App",
+                //   status: "Active",
+                //   role: "Admin",
+                //   last: "2018/03/29 @07:25",
+                //   every: "1 hour",
+                //   on: "Monday",
+                //   total: "10",
+                // },
+                // {
+                //   name: "Test App",
+                //   status: "Active",
+                //   role: "Admin",
+                //   last: "2018/03/29 @07:25",
+                //   every: "1 hour",
+                //   on: "Monday",
+                //   total: "10",
+                // },
+                // {
+                //   name: "Test App",
+                //   status: "Active",
+                //   role: "Admin",
+                //   last: "2018/03/29 @07:25",
+                //   every: "1 hour",
+                //   on: "Monday",
+                //   total: "10",
+                // },
+            ],
+        };
+    },
+    components: { AddAppManagerDialog }
 };
 </script>
